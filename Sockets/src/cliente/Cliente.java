ef8413e4a4aa7c1c8a18c0d2eaeb157356879164
Diego Castro Oliveros <94019700+Diegocastro21@users.noreply.github.com>
2022-05-28 17:02:21 -0500

    Subida de miniSockets

---
+++ Sockets/src/cliente/Cliente.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cliente;
+
+/**
+ *
+ * @author macbookprom1
+ */
+public class Cliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        jframeCliente  c = new jframeCliente();
+        c.setVisible(true);
+    }
+    
+}
---
+++ Sockets/src/servidor/Servidor.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package servidor;
+
+/**
+ *
+ * @author macbookprom1
+ */
+public class Servidor {
+    
+    public static void main(String[] args) {
+        
+        
+        jframeServidor s = new jframeServidor();
+        s.setVisible(true);
+        
+        
+    }
+    
+}
